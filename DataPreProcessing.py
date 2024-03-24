8f0a25e3c711902f1f5efe68fcc333fef0c6ff99
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2024-03-24 14:33:54 +0530

    Week 1 Initial

---
+++ DataPreProcessing.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+# Dealing with missing data
+
+# Check data collection source
+# Drop Missing Values
+# Replace the missing values with an average / frequency / other functions
+# Simply leave it as missing
+
+# dataframe.dropna() : Drop the row
+
+# dataframe.replace(missing_value,new_value)
+
+
+
+# Data Formatting
+
